5c1093637b3d36eca437c83d794c7bc41cdf1328
Anas Dorgham <dorghamprogrammer27@gmail.com>
2025-02-09 01:50:16 +0200

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// Scroll Reveal Animations
+ScrollReveal().reveal('.section', { delay: 300, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('.project-card', { delay: 500, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('.skill-card', { delay: 500, distance: '50px', origin: 'bottom', interval: 200 });
